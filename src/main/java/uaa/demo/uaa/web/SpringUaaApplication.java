1cc75446465ab02470c6edcfe071e0799444ecfc
ivanange <ivanangethe1@gmail.com>
2023-02-06 06:12:46 +0100

    first commit

---
+++ src/main/java/uaa/demo/uaa/web/SpringUaaApplication.java
@@ -0,0 +1,13 @@
+package uaa.demo.uaa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uaa/demo/uaa/web/SpringUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package uaa.demo.uaa.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
